19091efb8fb15e32ceae452a6df644e9232650d6
Tasco Silva <tascosilva@tasco.home>
2021-06-14 13:56:13 +0200

    Add error handlers.

--- app.py
+++ app.py
@@ -279,6 +279,22 @@ def delete_category(category_id):
     return redirect(url_for('profile', username=session["user"]))
 
 
+# Error handlers
+@app.errorhandler(403)
+def forbidden_access(e):
+    return render_template('/error_handlers/403.html'), 403
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('/error_handlers/404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_error(e):
+    return render_template('/error_handlers/500.html'), 500
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
     port=int(os.environ.get("PORT")),
