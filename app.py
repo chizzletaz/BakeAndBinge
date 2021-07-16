cda1e64600bf3530c30cb48f6ac7953917025d42
Tasco Silva <tascosilva@tasco.home>
2021-07-16 14:53:12 +0200

    Docs: update Testing.md.

--- app.py
+++ app.py
@@ -504,4 +504,4 @@ def internal_error(e):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=False)
+            debug=True)
